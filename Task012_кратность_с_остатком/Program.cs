fa15ed8ce30ee31dfcb7bba74fc4de3e99a12fcd
Evgeniy Uslinskiy <nothgot@gmail.com>
2023-10-13 17:06:26 +1000

    решено

---
+++ Task012_кратность_с_остатком/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1%number2 == 0)
+{
+  System.Console.WriteLine("кратно");
+}
+else
+{
+  int number3 = number1%number2;
+  System.Console.WriteLine($"некратно, остаток {number3}");
+}

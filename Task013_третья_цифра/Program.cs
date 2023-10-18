c3c7091cbf979c878a4d98267008a0c8e481a265
Evgeniy Uslinskiy <nothgot@gmail.com>
2023-10-18 14:06:48 +1000

    отображение 3 цифры

---
+++ Task013_третья_цифра/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number<100)
+{
+    System.Console.WriteLine("В числе нет 3 цифры");
+}
+else
+{
+    while (number>999)
+    {
+    number=number/10;
+    }
+    System.Console.WriteLine(number%10);
+}

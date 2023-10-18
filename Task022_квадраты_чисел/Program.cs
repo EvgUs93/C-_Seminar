8758a51aba1fbd97e75434d48410f9cb1a982b9c
Evgeniy Uslinskiy <nothgot@gmail.com>
2023-10-18 18:57:30 +1000

    квадратики

---
+++ Task022_квадраты_чисел/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+22. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу 
+квадратов чисел от 1 до N.
+
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4
+*/
+
+
+Console.Clear();
+System.Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+for (int i=1; i <= N; i++)
+{
+    if (i <N)
+    {
+         Console.Write(Math.Pow(i, 2)+", " );
+    }
+    else
+    {
+        Console.Write(Math.Pow(i, 2)+". " );
+    }
+}

b88b3dc5e8ad21f8b869d8b19cad40c8a3900eaa
Evgeniy Uslinskiy <nothgot@gmail.com>
2023-10-18 21:07:03 +1000

    расстояние в 3д

---
+++ Task021_расстояние_в_3D/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+21. Напишите программу, которая принимает на вход координаты двух точек и 
+находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+Console.Clear();
+
+System.Console.Write("Введите Х первой точки:");
+int FirstX = int.Parse(Console.ReadLine());
+System.Console.Write("Введите Y первой точки:");
+int FirstY = int.Parse(Console.ReadLine());
+System.Console.Write("Введите Z первой точки:");
+int FirstZ = int.Parse(Console.ReadLine());
+System.Console.Write("Введите Х второй точки:");
+int SecondX = int.Parse(Console.ReadLine());
+System.Console.Write("Введите Y второй точки:");
+int SecondY = int.Parse(Console.ReadLine());
+System.Console.Write("Введите Z второй точки:");
+int SecondZ = int.Parse(Console.ReadLine());
+                     //Math.Round(а, б) округляет а до б знаков после запятой
+double distanse = Math.Round((Math.Sqrt( //Math.Sqrt(а) извлекает корень а
+    Math.Pow((FirstX - SecondX), 2) + //Math.Pow (а, б) возводит а в степень б
+    Math.Pow((FirstY - SecondY), 2) +
+    Math.Pow((FirstZ - SecondZ), 2)
+    )), 2);
+System.Console.WriteLine($"расстояние равно {distanse}");

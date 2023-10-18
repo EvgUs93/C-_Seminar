9ec241c2eca52f2252638de306aee8c9f34afa56
Evgeniy Uslinskiy <nothgot@gmail.com>
2023-10-18 23:03:34 +1000

    большая цифра числа

---
+++ Task009_Большая_цифра_двузначного_числа/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+9. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+78 -> 8
+12-> 2
+85 -> 8
+*/
+
+int N = new Random(). Next(10, 100);
+System.Console.WriteLine(N);
+if (N/10 > N%10)
+System.Console.WriteLine(N/10);
+else
+System.Console.WriteLine(N%10);

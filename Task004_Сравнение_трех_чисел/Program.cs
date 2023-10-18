4dfd292a3ee72ec9ceb89363175ce1d7a83ed8f7
Evgeniy Uslinskiy <nothgot@gmail.com>
2023-10-18 22:19:50 +1000

    сравнение трех

---
+++ Task004_Сравнение_трех_чисел/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+4. Напишите программу, которая принимает на вход три числа и выдаёт 
+максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+System.Console.WriteLine("Введите первое число");
+int N1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+int N2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите Третье число");
+int N3 = Convert.ToInt32(Console.ReadLine());
+
+if (N1>N2)
+{
+    if (N1>N3)
+    {
+        System.Console.WriteLine($"самое большое число {N1}");
+    }
+    else
+    {
+        System.Console.WriteLine($"самое большое число {N3}");
+    }
+}
+else
+{
+    if (N2>N3)
+    {
+        System.Console.WriteLine($"самое большое число {N2}");
+    }
+    else
+    {
+        System.Console.WriteLine($"самое большое число {N3}");
+    }
+}

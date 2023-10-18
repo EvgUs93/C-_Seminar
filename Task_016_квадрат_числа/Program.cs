cc01d377dab0da24571d71488589e39954ce838f
Evgeniy Uslinskiy <nothgot@gmail.com>
2023-10-18 13:12:50 +1000

    квадрат числа

---
+++ Task_016_квадрат_числа/Program.cs
@@ -0,0 +1,20 @@
+﻿/*16.Напишите программу, которая принимает на вход два числа и проверяет,
+является ли одно число квадратом другого.
+5, 25 -> да
+-4, 16 -> да
+25, 5 -> да
+8,9 -> нет*/
+
+System.Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 * number1 == number2)
+{
+    System.Console.WriteLine($"число 'number2' является квадратом числа 'number1'");
+}
+else
+{
+    System.Console.WriteLine($"число {number2} не является квадратом числа {number1}");
+}

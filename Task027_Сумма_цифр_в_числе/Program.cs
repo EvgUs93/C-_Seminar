48f89c09fc25efc7a5c129803066a899e19411c4
Evgeniy Uslinskiy <nothgot@gmail.com>
2023-10-22 17:19:39 +1000

    task 027

---
+++ Task027_Сумма_цифр_в_числе/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму 
+цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+System.Console.WriteLine("Введите число");
+int user_num = int.Parse(Console.ReadLine());
+
+int Sum(int num)
+{
+    int result = 0;
+    for (int i = num; i > 0; i /= 10)
+    {
+       result += num%10;
+    num /=10;
+    }
+    return result;
+}
+
+int user_sum = Sum(user_num);
+System.Console.WriteLine($"Сумма цифр в числе {user_num} равна {user_sum}");

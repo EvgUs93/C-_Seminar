d3930cf9e962a33992440501568034ebd0bd76e8
Evgeniy Uslinskiy <nothgot@gmail.com>
2023-10-22 14:23:23 +1000

    цифр в числе

---
+++ Task026_Количество_цифр_в_числе/Program.cs
@@ -0,0 +1,22 @@
+﻿/*26. Напишите программу, которая принимает на вход число и выдаёт количество 
+цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+System.Console.WriteLine("Веедите число:");
+int user_num = int.Parse(Console.ReadLine());
+
+int Numb(int number)
+{
+    int result = 0;
+    for (int i = number; i > 0; i/=10)
+    {
+        result+=1;
+    }
+    return result;
+}
+
+int res = Numb(user_num);
+System.Console.WriteLine($"В числе {user_num} {res} цифр");

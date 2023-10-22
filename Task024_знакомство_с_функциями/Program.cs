a97d9dbbc30d95f5779178a4e897a081606fcd5f
Evgeniy Uslinskiy <nothgot@gmail.com>
2023-10-22 13:52:28 +1000

    sum method

---
+++ Task024_знакомство_с_функциями/Program.cs
@@ -0,0 +1,22 @@
+﻿/*24. Напишите программу, которая принимает на вход число (А) и выдаёт сумму 
+чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36
+*/
+
+System.Console.WriteLine("введите число");
+int user_num = int.Parse(Console.ReadLine());
+
+int Sum(int N)
+{
+    int result =0;
+    for (int i= 1; i <= N; i++)
+    {
+        result = result + i;
+    }
+    return result;
+}
+
+int sum = Sum(user_num);
+System.Console.WriteLine(sum);

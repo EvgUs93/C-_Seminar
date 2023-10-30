c565af9d8b6f9aa7feb0e80fe104cf97b1d642bb
Evgeniy Uslinskiy <nothgot@gmail.com>
2023-10-30 19:21:20 +1000

    task045

---
+++ Task045_Поэлементное_копирование/Program.cs
@@ -0,0 +1,42 @@
+﻿/*45. Напишите программу, которая будет создавать копию заданного одномерного 
+массива с помощью поэлементного копирования. 
+*/
+
+Console.Clear();
+int[] array = NewRND(5);
+int[] clone = Clone(array);
+Print(array);
+System.Console.WriteLine();
+Print(clone);
+
+int[] NewRND(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+    return array;
+}
+
+void Print(int[] array)
+{
+    foreach (int i in array)
+        System.Console.Write(i + "\t");
+}
+
+int[] Clone(int[] array)
+{
+    int[] array_clone = new int[array.Length];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array_clone[i] = array[i];
+    }
+    return array_clone;
+}
+
+
+
+
+
+

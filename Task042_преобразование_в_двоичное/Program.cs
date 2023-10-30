df14c5acbcb03f852104b153eb036fc7cc69e35a
Evgeniy Uslinskiy <nothgot@gmail.com>
2023-10-30 16:38:06 +1000

    task042

---
+++ Task042_преобразование_в_двоичное/Program.cs
@@ -0,0 +1,21 @@
+﻿/*42. Напишите программу, которая будет преобразовывать десятичное число в 
+двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10
+*/
+
+Console.Clear();
+void InBinaryCode(int num)
+{
+    string result = "";
+    int binary = 0;
+    for (int i = num; i > 0; i /= 2)
+    {
+        binary = i %2;
+        result = binary + result;
+    }
+    System.Console.WriteLine(result);
+}
+
+InBinaryCode(10);

b259f6ae0bd8f7a5aeab4586dee781c668da9eeb
Evgeniy Uslinskiy <nothgot@gmail.com>
2023-11-02 17:21:42 +1000

    task 046

---
+++ Task046_создание_двумерного_массива/Program.cs
@@ -0,0 +1,30 @@
+﻿/*46. Задайте двумерный массив размером m×n, заполненный случайными целыми 
+числами.*/
+
+int[,] getArray()
+{
+    int[,] array = new int[5, 5];
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void printArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] array = getArray();
+printArray(array);

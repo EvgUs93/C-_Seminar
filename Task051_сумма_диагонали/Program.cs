4b3c8c080c48f0178d8277e4b8a7f976c8b20759
Evgeniy Uslinskiy <nothgot@gmail.com>
2023-11-04 00:01:03 +1000

    task 051

---
+++ Task051_сумма_диагонали/Program.cs
@@ -0,0 +1,49 @@
+﻿/*51. Задайте двумерный массив. Найдите сумму элементов, находящихся на главной 
+диагонали (с индексами (0,0); (1;1) и т.д.
+*/
+
+Console.Clear();
+
+int[,] GetArray()
+{
+    int[,] array = new int[5, 5];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 11);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int Sum(int[,] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == j) result += array[i, j];
+        }
+    }
+    return result;
+}
+
+int[,] array2 = GetArray();
+PrintArray(array2);
+System.Console.WriteLine();
+int sum = Sum(array2);
+System.Console.WriteLine(sum);

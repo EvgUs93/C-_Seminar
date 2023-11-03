aaf8a6003087ca6eb3bd2b8ca75c5e392c955779
Evgeniy Uslinskiy <nothgot@gmail.com>
2023-11-03 23:22:02 +1000

    Task048

---
+++ Task048_двумерный_массив2/Program.cs
@@ -0,0 +1,35 @@
+﻿/*48. Задайте двумерный массив размера m на n, каждый элемент в массиве 
+находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+*/
+
+Console.Clear();
+
+int[,] GetArray()
+{
+    int[,] array = new int[5, 5];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] array = GetArray();
+PrintArray(array);
+
+

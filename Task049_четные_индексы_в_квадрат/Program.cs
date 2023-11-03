36158af408a38ed4e7a2db1c657aec7a1c1cab64
Evgeniy Uslinskiy <nothgot@gmail.com>
2023-11-03 23:54:10 +1000

    task 049

---
+++ Task049_четные_индексы_в_квадрат/Program.cs
@@ -0,0 +1,53 @@
+﻿/*Валерия Шарандина 49. Задайте двумерный массив. Найдите элементы, у которых 
+оба индекса чётные, и замените эти элементы на их квадраты.
+*/
+
+Console.Clear();
+
+int[,] GetArray()
+{
+    int[,] array = new int[5, 5];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 11);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] Pow(int[,] array)
+{
+    int x = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if((i != 0) &&( j != 0))
+            {
+                array[i, j] = array[i, j] * array[i, j];
+            }
+        }
+    }
+    return array;
+}
+
+int[,] array = GetArray();
+PrintArray(array);
+System.Console.WriteLine();
+int[,] array1 = Pow(array);
+PrintArray(array1);
+

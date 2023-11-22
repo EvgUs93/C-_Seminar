9922bf00281686567615d1766009f312ec0c03ae
Evgeniy Uslinskiy <nothgot@gmail.com>
2023-11-22 19:47:26 +1000

    Task 066

---
+++ Task_066_от_M_до_N_сумма_рекурсия/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.Clear();
+int sum = Summ(4, 8);
+System.Console.WriteLine(sum);
+int Summ(int start, int finish)
+{
+    if ( start > finish)
+    return 0;
+    else
+    {
+        return start + Summ( start +1, finish);
+    }
+}

a7b29891aa07096e3c6e3936d6fd1ddf1bb75d9b
Evgeniy Uslinskiy <nothgot@gmail.com>
2023-11-22 18:56:34 +1000

    task 065

---
+++ Task_065_от_M_до_N_рекурсия/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+65. Задайте значения M и N. Напишите программу, которая выведет все натуральные 
+числа в промежутке от M до N.
+*/
+
+Console.Clear();
+Diapason(4, 10);
+
+void Diapason(int M, int N)
+{
+if ( M > N) return;
+else
+{
+    System.Console.Write(M + " ");
+    Diapason(M + 1, N);
+}
+}
+

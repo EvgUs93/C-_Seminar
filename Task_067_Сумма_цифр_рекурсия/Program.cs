fdf7f5989a0cadd4f526e696f37eac11cb649456
Evgeniy Uslinskiy <nothgot@gmail.com>
2023-11-22 19:13:15 +1000

    task 67

---
+++ Task_067_Сумма_цифр_рекурсия/Program.cs
@@ -0,0 +1,17 @@
+﻿/*67. Напишите программу, которая будет принимать на вход число и возвращать 
+сумму его цифр.
+*/
+
+Console.Clear();
+int sum = Summ(666);
+System.Console.WriteLine(sum);
+int Summ(int numb)
+{
+    int result = 0;
+    if ( numb == 0)
+    return 0;
+    else
+    {
+        return numb % 10 + Summ( numb / 10);
+    }
+}

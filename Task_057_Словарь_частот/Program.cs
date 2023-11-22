7fa0c3179871e9095d7169fef7f3b0a566430359
Evgeniy Uslinskiy <nothgot@gmail.com>
2023-11-22 15:51:37 +1000

    task 55111

--- Task_057_Словарь_частот/Program.cs
+++ Task_057_Словарь_частот/Program.cs
@@ -69,3 +69,55 @@ void FindCount(int[,] array)
         System.Console.WriteLine($"Число {m} встречается {count} раз.");
     }
 }
+/*
+Решение Романа, через Лист
+int[,] GetRnd(int rows, int colom)
+{
+int[,] array = new int[rows, colom];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+array[i, j] = new Random().Next(0, 10);
+}
+}
+return array;
+}
+
+int[,] testArray = GetRnd(5, 5);
+
+for (int i = 0; i < testArray.GetLength(0); i++)
+{
+for (int j = 0; j < testArray.GetLength(1); j++)
+{
+Console.Write(testArray[i, j] + " ");
+}
+Console.WriteLine();
+}
+
+
+PrintElementsCount(testArray);
+
+
+
+void PrintElementsCount(int[,] arr)
+{
+List<int> list = new List<int>();
+
+
+foreach (int item in arr)
+{
+if (!list.Contains(item))
+{
+int count = 0;
+foreach (int checkitem in arr)
+{
+if (item == checkitem) count++;
+}
+
+System.Console.WriteLine(item + " встречается " + count + " раз");
+}
+list.Add(item);
+}
+}
+*/

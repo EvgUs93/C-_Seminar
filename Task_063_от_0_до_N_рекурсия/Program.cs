9ca0ec2c87b91691a82a9b9f5314be481f11cd51
Evgeniy Uslinskiy <nothgot@gmail.com>
2023-11-22 18:47:57 +1000

    task063

---
+++ Task_063_от_0_до_N_рекурсия/Program.cs
@@ -0,0 +1,24 @@
+﻿/*63. Задайте значение N. Напишите программу, которая выведет все натуральные 
+числа в промежутке от 1 до N.
+*/
+Console.Clear();
+int usernum = UserNum("Введите число");
+Print(usernum);
+
+int UserNum(string message)
+{
+    System.Console.WriteLine(message);
+    int userN = int.Parse(Console.ReadLine());
+    return userN;
+}
+
+void Print(int number, int start = 1)
+{
+    if (number < start)
+    return;
+    else
+    {
+        System.Console.Write(start + " ");
+        Print(number, start + 1);
+    }
+}

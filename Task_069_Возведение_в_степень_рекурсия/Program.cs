8ee60e66a9967e607e188e551da6fc865d4ecbf7
Evgeniy Uslinskiy <nothgot@gmail.com>
2023-11-22 19:24:59 +1000

    task 69

---
+++ Task_069_Возведение_в_степень_рекурсия/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+69. Напишите программу, которая на вход принимает два числа A и B, и возводит 
+число А в целую степень B с помощью рекурсии.
+*/
+
+Console.Clear();
+int test = Pow(5, 3);
+System.Console.WriteLine(test);
+
+int Pow(int number, int square)
+{
+    if ( square == 1)
+    {
+        return number;
+    }
+    else
+    {
+        return number * Pow(number, square - 1);
+    }
+}
